f2a82e5e526de9d681af59564b498c04ffec5fda
LarrySnyder <larrysnyder610@gmail.com>
2019-03-12 21:20:42 -0400

    First cut at newsvendor function

--- inventory/newsvendor.py
+++ inventory/newsvendor.py
@@ -6,6 +6,7 @@ Lehigh University and Opex Analytics
 """
 
 import numpy as np
+from scipy import stats
 
 
 def newsvendor(holding_cost, stockout_cost, demand_mean, demand_sd):
@@ -15,31 +16,37 @@ def newsvendor(holding_cost, stockout_cost, demand_mean, demand_sd):
 
 	Parameters
 	----------
-	fixed_cost : float
-		Fixed cost per order. [K]
 	holding_cost : float
-		Holding cost per item per unit time. [h]
-	demand_rate : float
-		Demand (items) per unit time. [lambda]
+		Holding cost per item per period. [h]
+	stockout_cost : float
+		Stockout cost per item per period. [p]
+	demand_mean : float
+		Mean demand per period. [mu]
+	demand_sd : float
+		Standard deviation of demand per period. [sigma]
 
 	Returns
 	-------
-	order_quantity : float
-		Optimal order quantity (items). [Q^*]
+	base_stock_level : float
+		Optimal base-stock level. [S^*]
 	cost : float
-		Optimal cost per unit time. [g^*]
+		Optimal cost per period. [g^*]
 	"""
 
 	# Check that parameters are positive.
-	assert fixed_cost > 0, "fixed_cost must be positive."
 	assert holding_cost > 0, "holding_cost must be positive."
-	assert demand_rate > 0, "demand_rate must be positive."
+	assert stockout_cost > 0, "stockout_cost must be positive."
+	assert demand_mean > 0, "demand_mean must be positive."
+	assert demand_sd > 0, "demand_sd must be positive."
+
+	# Calculate alpha.
+	alpha = stockout_cost / (stockout_cost + holding_cost)
 
 	# Calculate optimal order quantity and cost.
-	order_quantity = np.sqrt(2 * fixed_cost * demand_rate / holding_cost)
-	cost = order_quantity * holding_cost
+	base_stock_level = stats.norm.ppf(alpha, demand_mean, demand_sd)
+	cost = (holding_cost + stockout_cost) * stats.norm.pdf(stats.norm.ppf(alpha, 0, 1)) * demand_sd
 
-	return order_quantity, cost
+	return base_stock_level, cost
 
 
 def economic_order_quantity_with_backorders(fixed_cost, holding_cost, stockout_cost, demand_rate):
