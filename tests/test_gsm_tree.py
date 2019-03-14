472da31b6b51d8b389e545efc1781713f664ab2f
LarrySnyder <larrysnyder610@gmail.com>
2019-03-14 15:57:31 -0400

    Add setUp and tearDown methods to test_ module

--- tests/test_gsm_tree.py
+++ tests/test_gsm_tree.py
@@ -5,27 +5,82 @@ import networkx as nx
 from inventory import gsm_tree
 
 
+class GSMTreeData():
+	"""Contains data for GSM tree algorithms.
+
+	TODO: for now I am putting this in test_ module, but at some point it should probably be defined elsewhere.
+	"""
+
+	def __init__(self):
+		self.tree = None
+		self.processing_times = None
+		self.external_lead_times = None
+		self.holding_costs = None
 
 
+# Class-level data objects. (Data will be filled in setUp functions.)
+instance_figure_6_12 = GSMTreeData()
+instance_example_6_5 = GSMTreeData()
 
+
+def setUpModule():
+	"""Called once, before anything else in this module."""
+	log_point('module %s' % __name__)
+
+	# Build instance corresponding to network in Figure 6.12.
+	instance_figure_6_12.tree = nx.DiGraph()
+	instance_figure_6_12.tree.add_nodes_from(range(1, 8))
+	instance_figure_6_12.tree.add_edge(1, 2)
+	instance_figure_6_12.tree.add_edge(1, 3)
+	instance_figure_6_12.tree.add_edge(3, 5)
+	instance_figure_6_12.tree.add_edge(4, 5)
+	instance_figure_6_12.tree.add_edge(5, 6)
+	instance_figure_6_12.tree.add_edge(5, 7)
+
+
+def tearDownModule():
+	"""Called once, after everything else in this module."""
+	log_point('module %s' % __name__)
+
+
+def log_point(context):
+	"""Utility function to trace control flow, for module functions and class methods.
+
+	http://pythontesting.net/framework/unittest/unittest-fixtures/
+	"""
+	calling_function = inspect.stack()[1][3]
+	print("in {:s} - {:s}()".format(context, calling_function))
 
 
 class TestRelabelNodes(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		"""Called once, before any tests."""
+		log_point("class {:s}".format(cls.__name__))
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Called once, after all tests, if setUpClass successful."""
+		log_point("class {:s}".format(cls.__name__))
+
+	def log_point(self):
+		"""Utility method to trace control flow.
+
+		http://pythontesting.net/framework/unittest/unittest-fixtures/
+		"""
+		calling_function = inspect.stack()[1][3]
+		current_test = self.id().split('.')[-1]
+		print("\nin {:s} - {:s}()".format(current_test, calling_function))
+
 	def test_figure_6_12(self):
 		"""Test that relabel_nodes()  correctly relabels network
 		in Figure 6.12.
 		"""
 
-		# Network from Figure 6.12.
-		G = nx.DiGraph()
-		G.add_nodes_from(range(1, 8))
-		G.add_edge(1, 2)
-		G.add_edge(1, 3)
-		G.add_edge(3, 5)
-		G.add_edge(4, 5)
-		G.add_edge(5, 6)
-		G.add_edge(5, 7)
-		new_G, new_labels = gsm_tree.relabel_nodes(G, start_index=1)
+		self.log_point()
+
+		new_G, new_labels = gsm_tree.relabel_nodes(figure_6_12_tree, start_index=1)
 
 		# Build correct relabeled network, and list of correct labels.
 		correct_G = nx.DiGraph()
@@ -43,21 +98,34 @@ class TestRelabelNodes(unittest.TestCase):
 
 
 class TestFindLargerAdjacentNodes(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		"""Called once, before any tests."""
+		log_point('class %s' % cls.__name__)
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Called once, after all tests, if setUpClass successful."""
+		log_point('class %s' % cls.__name__)
+
+	def log_point(self):
+		"""Utility method to trace control flow.
+
+		http://pythontesting.net/framework/unittest/unittest-fixtures/
+		"""
+		calling_function = inspect.stack()[1][3]
+		current_test = self.id().split('.')[-1]
+		print("\nin {:s} - {:s}()".format(current_test, calling_function))
+
 	def test_figure_6_12(self):
 		"""Test that find_larger_adjacent_nodes() works for relabeled network
 		in Figure 6.12.
 		"""
 
-		# Network from Figure 6.12.
-		G = nx.DiGraph()
-		G.add_nodes_from(range(1, 8))
-		G.add_edge(1, 2)
-		G.add_edge(1, 3)
-		G.add_edge(3, 5)
-		G.add_edge(4, 5)
-		G.add_edge(5, 6)
-		G.add_edge(5, 7)
-		new_G, _ = gsm_tree.relabel_nodes(G, start_index=1)
+		self.log_point()
+
+		new_G, _ = gsm_tree.relabel_nodes(figure_6_12_tree, start_index=1)
 		larger_adjacent, downstream = gsm_tree.find_larger_adjacent_nodes(new_G)
 
 		# Build correct dictionaries
