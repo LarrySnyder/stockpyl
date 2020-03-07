7d42e2554a83965321a6d12412466baa227b66fd
LarrySnyder <larrysnyder610@gmail.com>
2020-03-06 19:48:50 -0500

    More implementing Policy class

--- inventory/policy.py
+++ inventory/policy.py
@@ -28,6 +28,7 @@ class InventoryPolicyType(Enum):
 	BASE_STOCK = 0
 	r_Q = 1
 	s_S = 2
+	FIXED_QUANTITY = 3
 #	STERMAN = 3
 #	RANDOM = 4
 
@@ -37,28 +38,28 @@ class InventoryPolicyType(Enum):
 # ===============================================================================
 
 class Policy(object):
-	"""
-	The ``Policy`` class.
+	"""The ``Policy`` class.
 
-	:arg InventoryPolicyType policy_type:
+	Parameters
+	----------
+	policy_type : InventoryPolicyType
 		The inventory policy type.
-	:arg float param1:
+	param1 : float
 		First inventory parameter; usage depends on policy_type.
-	:arg float param2:
+	param2 : float
 		Second inventory parameter; usage depends on policy_type.
-
 	"""
 	def __init__(self, policy_type=None, param1=None, param2=None):
-		"""
-		Card constructor method.
+		"""Policy constructor method.
 
-		:arg InventoryPolicyType policy_type:
+		Parameters
+		----------
+		policy_type : InventoryPolicyType
 			The inventory policy type.
-		:arg float param1:
+		param1 : float
 			First inventory parameter; usage depends on policy_type.
-		:arg float param2:
+		param2 : float
 			Second inventory parameter; usage depends on policy_type.
-
 		"""
 		# Initialize parameters to None. (Relevant parameters will be filled
 		# below.)
@@ -70,22 +71,64 @@ class Policy(object):
 		# Set policy_type.
 		self.policy_type = policy_type
 
-		# Set policy parameters.
+		# Set (and validate) policy parameters.
 		if policy_type == InventoryPolicyType.BASE_STOCK:
 			self.base_stock_level = param1
 		elif policy_type == InventoryPolicyType.r_Q:
 			self.reorder_point = param1
 			self.order_quantity = param2
+			assert param2 >= 0, "For r_Q policy, param2 (order quantity) must be non-negative"
 		elif policy_type == InventoryPolicyType.s_S:
 			self.reorder_point = param1
 			self.order_up_to_level = param2
+			assert param1 <= param2, "For s_S policy, param1 (reorder point) must be <= param2 (order-up-to level)"
+		elif policy_type == InventoryPolicyType.FIXED_QUANTITY:
+			self.order_quantity = param1
+			assert param1 >= 0, "for FIXED_QUANTITY policy, param1 (order quantity) must be non-negative"
+
+	# # Properties.
+	#
+	# @property
+	# def base_stock_level(self):
+	# 	return self._base_stock_level
+	#
+	# @base_stock_level.setter
+	# def base_stock_level(self, value):
+	# 	self._base_stock_level = value
+	#
+	# @property
+	# def reorder_point(self):
+	# 	return self._reorder_point
+	#
+	# @reorder_point.setter
+	# def reorder_point(self, value):
+	# 	self._reorder_point = value
+	#
+	# @property
+	# def order_quantity(self):
+	# 	return self._order_quantity
+	#
+	# @order_quantity.setter
+	# def order_quantity(self, value):
+	# 	self._order_quantity = value
+	#
+	# @property
+	# def order_up_to_level(self):
+	# 	return self._order_up_to_level
+	#
+	# @order_up_to_level.setter
+	# def order_up_to_level(self, value):
+	# 	self._order_up_to_level = value
+
+	# Other functions.
 
 	def __repr__(self):
 		"""
-		Returns a string representation of the ``Policy`` instance.
+		Return a string representation of the ``Policy`` instance.
 
-		:returns:
-			A string representation of the Policy instance.
+		Returns
+		-------
+			A string representation of the ``Policy`` instance.
 
 		"""
 		# Build string of parameters.
@@ -106,13 +149,103 @@ class Policy(object):
 
 	def __str__(self):
 		"""
-		Returns the full name of the ``Policy`` instance.
+		Return the full name of the ``Policy`` instance.
 
-		:returns:
+		Returns
+		-------
 			The policy name.
 
 		"""
 		return self.__repr__()
 
+	def get_order_quantity(self, inventory_position=None):
+		"""Calculate order quantity using the inventory policy type specified
+		in policy_type. The following parameters must be specified:
+			- BASE_STOCK: inventory_position
+			- r_Q: inventory_position
+			- s_S: inventory_position
 
+		Returns
+		-------
+		order_quantity : float
+			The order quantity.
+
+		"""
+
+		if self.policy_type == InventoryPolicyType.BASE_STOCK:
+			return self.get_order_quantity_base_stock(inventory_position)
+		elif self.policy_type == InventoryPolicyType.r_Q:
+			return self.get_order_quantity_r_Q(inventory_position)
+		elif self.policy_type == InventoryPolicyType.s_S:
+			return self.get_order_quantity_s_S(inventory_position)
+		elif self.policy_type == InventoryPolicyType.FIXED_QUANTITY:
+			return self.get_order_quantity_fixed_quantity()
+
+	def get_order_quantity_base_stock(self, inventory_position):
+		"""Calculate order quantity using a base-stock policy.
+
+		Parameters
+		----------
+		inventory_position : float
+			Inventory position immediately before order is placed.
+
+		Returns
+		-------
+		order_quantity : float
+			The order quantity.
+
+		"""
+		return max(0.0, self.base_stock_level - inventory_position)
+
+	def get_order_quantity_r_Q(self, inventory_position):
+		"""Calculate order quantity using an (r,Q) policy.
+
+		Parameters
+		----------
+		inventory_position : float
+			Inventory position immediately before order is placed.
+
+		Returns
+		-------
+		order_quantity : float
+			The order quantity.
+
+		"""
+		if inventory_position <= self.reorder_point:
+			return self.order_quantity
+		else:
+			return 0.0
+
+	def get_order_quantity_s_S(self, inventory_position):
+		"""Calculate order quantity using an (s,S) policy.
+
+		Parameters
+		----------
+		inventory_position : float
+			Inventory position immediately before order is placed.
+
+		Returns
+		-------
+		order_quantity : float
+			The order quantity.
+
+		"""
+		if inventory_position <= self.reorder_point:
+			return self.order_up_to_level - inventory_position
+		else:
+			return 0
+
+	def get_order_quantity_fixed_quantity(self):
+		"""Calculate order quantity using a fixed-quantity policy.
+
+		Parameters
+		----------
+
+		Returns
+		-------
+		order_quantity : float
+			The order quantity.
+
+		"""
+		return self.order_quantity
 
--- tests/test_policy.py
+++ tests/test_policy.py
@@ -25,6 +25,33 @@ def tearDownModule():
 	print_status('---', 'tearDownModule()')
 
 
+class TestPolicyInit(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		"""Called once, before any tests."""
+		print_status('TestPolicyInit', 'setUpClass()')
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Called once, after all tests, if setUpClass successful."""
+		print_status('TestPolicyInit', 'tearDownClass()')
+
+	def test_init(self):
+		"""Test that Policy() correctly raises errors on incorrect parameters.
+		"""
+		print_status('TestPolicyInit', 'test_init()')
+
+		# Negative order quantities.
+		with self.assertRaises(AssertionError):
+			_ = Policy(InventoryPolicyType.r_Q, 100, -5)
+		with self.assertRaises(AssertionError):
+			_ = Policy(InventoryPolicyType.FIXED_QUANTITY, -100)
+
+		# s > S.
+		with self.assertRaises(AssertionError):
+			_ = Policy(InventoryPolicyType.s_S, 50, 40)
+
+
 class TestPolicyRepr(unittest.TestCase):
 	@classmethod
 	def setUpClass(cls):
@@ -37,7 +64,7 @@ class TestPolicyRepr(unittest.TestCase):
 		print_status('TestPolicyRepr', 'tearDownClass()')
 
 	def test_base_stock(self):
-		"""Test that policy.__repr__() correctly displays policy string for
+		"""Test that policy.__repr__() correctly returns policy string for
 		base-stock policy.
 		"""
 		print_status('TestPolicyRepr', 'test_base_stock()')
@@ -47,7 +74,7 @@ class TestPolicyRepr(unittest.TestCase):
 		self.assertEqual(policy_str, "Policy(BASE_STOCK: base_stock_level=105.30)")
 
 	def test_r_Q(self):
-		"""Test that policy.__repr__() correctly displays policy string for
+		"""Test that policy.__repr__() correctly returns policy string for
 		(r,Q) policy.
 		"""
 		print_status('TestPolicyRepr', 'test_r_Q()')
@@ -57,7 +84,7 @@ class TestPolicyRepr(unittest.TestCase):
 		self.assertEqual(policy_str, "Policy(r_Q: reorder_point=45.30, order_quantity=17.40)")
 
 	def test_s_S(self):
-		"""Test that policy.__repr__() correctly displays policy string for
+		"""Test that policy.__repr__() correctly returns policy string for
 		(r,Q) policy.
 		"""
 		print_status('TestPolicyRepr', 'test_s_S()')
@@ -79,7 +106,7 @@ class TestPolicyStr(unittest.TestCase):
 		print_status('TestPolicyStr', 'tearDownClass()')
 
 	def test_base_stock(self):
-		"""Test that policy.__str__() correctly displays policy string for
+		"""Test that policy.__str__() correctly returns policy string for
 		base-stock policy.
 		"""
 		print_status('TestPolicyStr', 'test_base_stock()')
@@ -89,7 +116,7 @@ class TestPolicyStr(unittest.TestCase):
 		self.assertEqual(policy_str, "Policy(BASE_STOCK: base_stock_level=105.30)")
 
 	def test_r_Q(self):
-		"""Test that policy.__str__() correctly displays policy string for
+		"""Test that policy.__str__() correctly returns policy string for
 		(r,Q) policy.
 		"""
 		print_status('TestPolicyStr', 'test_r_Q()')
@@ -99,7 +126,7 @@ class TestPolicyStr(unittest.TestCase):
 		self.assertEqual(policy_str, "Policy(r_Q: reorder_point=45.30, order_quantity=17.40)")
 
 	def test_s_S(self):
-		"""Test that policy.__repr__() correctly displays policy string for
+		"""Test that policy.__repr__() correctly returns policy string for
 		(r,Q) policy.
 		"""
 		print_status('TestPolicyStr', 'test_s_S()')
@@ -107,4 +134,168 @@ class TestPolicyStr(unittest.TestCase):
 		policy = Policy(InventoryPolicyType.s_S, 45.3, 117.4)
 		policy_str = policy.__str__()
 		self.assertEqual(policy_str, "Policy(s_S: reorder_point=45.30, order_up_to_level=117.40)")
+
+
+class TestGetOrderQuantity(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		"""Called once, before any tests."""
+		print_status('TestGetOrderQuantity', 'setUpClass()')
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Called once, after all tests, if setUpClass successful."""
+		print_status('TestGetOrderQuantity', 'tearDownClass()')
+
+	def test_base_stock(self):
+		"""Test that get_order_quantity() returns correct order quantity
+		under a base-stock policy for a few instances.
+		"""
+		print_status('TestGetOrderQuantity', 'test_base_stock()')
+
+		policy = Policy(InventoryPolicyType.BASE_STOCK, 100)
+		q1 = policy.get_order_quantity(85)
+		self.assertEqual(q1, 15)
+		q2 = policy.get_order_quantity(-20)
+		self.assertEqual(q2, 120)
+		q3 = policy.get_order_quantity(140)
+		self.assertEqual(q3, 0)
+
+	def test_r_Q(self):
+		"""Test that get_order_quantity() returns correct order quantity
+		under an (r,Q) policy for a few instances.
+		"""
+		print_status('TestGetOrderQuantity', 'test_r_Q()')
+
+		policy = Policy(InventoryPolicyType.r_Q, 100, 200)
+		q1 = policy.get_order_quantity(75)
+		self.assertEqual(q1, 200)
+		q2 = policy.get_order_quantity(-20)
+		self.assertEqual(q2, 200)
+		q3 = policy.get_order_quantity(140)
+		self.assertEqual(q3, 0)
+
+	def test_s_S(self):
+		"""Test that get_order_quantity() returns correct order quantity
+		under an (s,S) policy for a few instances.
+		"""
+		print_status('TestGetOrderQuantity', 'test_s_S()')
+
+		policy = Policy(InventoryPolicyType.s_S, 50, 100)
+		q1 = policy.get_order_quantity_s_S(15)
+		self.assertEqual(q1, 85)
+		q2 = policy.get_order_quantity_s_S(-20)
+		self.assertEqual(q2, 120)
+		q3 = policy.get_order_quantity_s_S(70)
+		self.assertEqual(q3, 0)
+
+	def test_fixed_quantity(self):
+		"""Test that get_order_quantity() returns correct order quantity
+		under a fixed-quantity policy.
+		"""
+		print_status('TestGetOrderQuantity', 'test_fixed_quantity()')
+
+		policy = Policy(InventoryPolicyType.FIXED_QUANTITY, 100)
+		q1 = policy.get_order_quantity_fixed_quantity()
+		self.assertEqual(q1, 100)
+
+
+class TestGetOrderQuantityBaseStock(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		"""Called once, before any tests."""
+		print_status('TestGetOrderQuantityBaseStock', 'setUpClass()')
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Called once, after all tests, if setUpClass successful."""
+		print_status('TestGetOrderQuantityBaseStock', 'tearDownClass()')
+
+	def test(self):
+		"""Test that get_order_quantity_base_stock() returns correct order
+		quantity for a few instances.
+		"""
+		print_status('TestGetOrderQuantityBaseStock', 'test()')
+
+		policy = Policy(InventoryPolicyType.BASE_STOCK, 100)
+		q1 = policy.get_order_quantity_base_stock(85)
+		self.assertEqual(q1, 15)
+		q2 = policy.get_order_quantity_base_stock(-20)
+		self.assertEqual(q2, 120)
+		q3 = policy.get_order_quantity_base_stock(140)
+		self.assertEqual(q3, 0)
+
+
+class TestGetOrderQuantityrQ(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		"""Called once, before any tests."""
+		print_status('TestGetOrderQuantityrQ', 'setUpClass()')
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Called once, after all tests, if setUpClass successful."""
+		print_status('TestGetOrderQuantityrQ', 'tearDownClass()')
+
+	def test(self):
+		"""Test that get_order_quantity_r_Q() returns correct order
+		quantity for a few instances.
+		"""
+		print_status('TestGetOrderQuantityrQ', 'test()')
+
+		policy = Policy(InventoryPolicyType.r_Q, 100, 200)
+		q1 = policy.get_order_quantity_r_Q(75)
+		self.assertEqual(q1, 200)
+		q2 = policy.get_order_quantity_r_Q(-20)
+		self.assertEqual(q2, 200)
+		q3 = policy.get_order_quantity_r_Q(140)
+		self.assertEqual(q3, 0)
+
+
+class TestGetOrderQuantitysS(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		"""Called once, before any tests."""
+		print_status('TestGetOrderQuantitysS', 'setUpClass()')
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Called once, after all tests, if setUpClass successful."""
+		print_status('TestGetOrderQuantitysS', 'tearDownClass()')
+
+	def test(self):
+		"""Test that get_order_quantity_s_S() returns correct order
+		quantity for a few instances.
+		"""
+		print_status('TestGetOrderQuantitysS', 'test()')
+
+		policy = Policy(InventoryPolicyType.s_S, 50, 100)
+		q1 = policy.get_order_quantity_s_S(15)
+		self.assertEqual(q1, 85)
+		q2 = policy.get_order_quantity_s_S(-20)
+		self.assertEqual(q2, 120)
+		q3 = policy.get_order_quantity_s_S(70)
+		self.assertEqual(q3, 0)
+
+
+class TestGetOrderQuantityFixedQuantity(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		"""Called once, before any tests."""
+		print_status('TestGetOrderQuantityFixedQuantity', 'setUpClass()')
+
+	@classmethod
+	def tearDownClass(cls):
+		"""Called once, after all tests, if setUpClass successful."""
+		print_status('TestGetOrderQuantityFixedQuantity', 'tearDownClass()')
+
+	def test(self):
+		"""Test that get_order_quantity_fixed_quantity() returns correct order
+		quantity.
+		"""
+		print_status('TestGetOrderQuantityFixedQuantity', 'test()')
+
+		policy = Policy(InventoryPolicyType.FIXED_QUANTITY, 100)
+		q1 = policy.get_order_quantity_fixed_quantity()
+		self.assertEqual(q1, 100)
 
